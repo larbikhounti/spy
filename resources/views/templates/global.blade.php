eb4603b9c5812173739c2d904a72a31263c8fb54
mohamed khounti <larbikhounti@gmail.com>
2024-01-20 14:11:17 +0100

    fixing css

--- resources/views/templates/global.blade.php
+++ resources/views/templates/global.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Default Title')</title>
     <link rel="stylesheet" href="{{ asset('css/output.css') }}">
+    @vite('resources/css/app.css')
     </head>
 
 <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
-  content: ["index.html", "./node_modules/flowbite/**/*.js"],
+  content: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+],
   theme: {
     extend: {
       colors: {
         primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" },
-        google: { "50": "#f5f5f5", "100": "#eeeeee", "200": "#e0e0e0", "300": "#bdbdbd", "400": "#9e9e9e", "500": "#757575", "600": "#616161", "700": "#424242", "800": "#212121", "900": "#212121" }
+        secondary: { "50": "#fdf2f8", "100": "#fce7f3", "200": "#fbcfe8", "300": "#f9a8d4", "400": "#f472b6", "500": "#ec4899", "600": "#db2777", "700": "#be185d", "800": "#9d174d", "900": "#831843" },
+        red: { "50": "#fef2f2", "100": "#fee2e2", "200": "#fecaca", "300": "#fca5a5", "400": "#f87171", "500": "#ef4444", "600": "#dc2626", "700": "#b91c1c", "800": "#991b1b", "900": "#7f1d1d" },
       }
     },
   },
