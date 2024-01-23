f2bcc4864b52d5d95f40279fa0cffa178ed38c87
mohamed khounti <larbikhounti@gmail.com>
2024-01-23 17:27:32 +0100

    fixing position

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -72,12 +72,13 @@
             
                 <div
                     class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="{{ asset('images/4/1/screenshot.png') }}" class="block h-48 w-96" style="background-image: url({{ asset('images/4/1/screenshot.png') }})">
+                    <a href="{{ asset('images/4/1/screenshot.png') }}" class="block h-48 w-96 bg-top" style="background-image: url({{ asset('images/4/1/screenshot.png') }})">
                       
                     </a>
                     <div class="p-5">
                         <a href="#">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Saudi Arabia
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {{$country->name}}
                             </h5>
                         </a>
                         <a href="{{ asset('images/4/1/screenshot.png') }}"
